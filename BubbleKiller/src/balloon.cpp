e020eaa00f0e65fac72024fd10d9e5f840dbef52
Mikhail Mitrofanov <mikhail404@gmail.com>
2015-11-04 22:51:20 +0300

    comments have been added to the illogical places

--- BubbleKiller/src/balloon.cpp
+++ BubbleKiller/src/balloon.cpp
@@ -4,14 +4,16 @@
 
 Balloon::Balloon(int id, Position start_position, double coef, double speed, int radius, Balloon::BALLOON_TYPE type)
     : m_id(id),
-      m_start_position(start_position),
+      m_center_point(start_position),
       m_coefficient(coef),
       m_speed(speed),
       m_radius(radius),
-      m_balloon_type(type),
-      m_center_point(start_position)
+      m_balloon_type(type)
+
 {
+    // if balloon has normal type
     m_color = std::make_tuple(0, 255, 0);
+    // ... or bomb type
     if (m_balloon_type == BOMB)
         m_color = std::make_tuple(0, 0, 0);
 }
@@ -38,8 +40,10 @@ int Balloon::radius() const
 
 void Balloon::check_position(const int &pos)
 {
+    // for cycle trace
     if (m_center_point.second < 0)
         m_center_point.second = pos;
+    // if you don't want such a cycle then remove current object
 }
 
 int Balloon::x() const
@@ -55,16 +59,6 @@ int Balloon::y() const
 Balloon::BALLOON_TYPE Balloon::balloon_type() const
 {
     return m_balloon_type;
-}
-
-void Balloon::print() const
-{
-    std::cout << m_id << std::endl;
-}
-
-bool Balloon::is_bang() const
-{
-    return false;
 }
 
 std::tuple <int, int, int> Balloon::color() const

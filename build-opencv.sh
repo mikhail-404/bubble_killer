62e7d1a0972a4bb3c0044458dcadd3ef1af07859
Kirill Pilyugin <kirillpilyugin@gmail.com>
2015-11-08 23:28:30 +0300

    fix

--- build-opencv.sh
+++ build-opencv.sh
@@ -3,6 +3,7 @@
 apt-get install libgtk2.0-dev
 
 wget http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/3.0.0/opencv-3.0.0.zip
+unzip opencv-3.0.0.zip
 cd opencv-3.0.0
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ./
 make
